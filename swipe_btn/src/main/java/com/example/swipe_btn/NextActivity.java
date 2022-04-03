6fb4c457b6a9aca5937782ce585153a616492115
AMAD <alaminthespecial@gmail.com>
2022-04-03 22:12:52 +0530

    Initial commit

---
+++ swipe_btn/src/main/java/com/example/swipe_btn/NextActivity.java
@@ -0,0 +1,14 @@
+package com.example.swipe_btn;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next);
+    }
+}
---
+++ swipe_btn/src/main/java/com/example/swipe_btn/OnActiveListener.java
@@ -0,0 +1,5 @@
+package com.example.swipe_btn;
+
+public interface OnActiveListener {
+    void onActive();
+}
